5e2ce54fc6ba3b07b03873301596f4bf698953f9
nramburrun <nsrambur@uwaterloo.ca>
2016-09-06 20:14:54 -0400

    Vectorized version of gradient descent

--- gradientDescent.m
+++ gradientDescent.m
@@ -11,20 +11,27 @@ sum_theta_zero=0;
 for iter = 1:num_iters
 
     % Perform gradient descent using batch update algorithm. Simultaneously update theta0 and theta 1
-    for m_its = 1:m
-        sum_theta_zero = sum_theta_zero + ( theta(1) + theta(2)*X(m_its,2) - y(m_its));
-    end
+%             for m_its = 1:m
+%                 sum_theta_zero = sum_theta_zero + ( theta(1) + theta(2)*X(m_its,2) - y(m_its));
+%             end
+% 
+%             for m_its = 1:m
+%                 sum_theta_one = sum_theta_one + ( theta(1) + theta(2)*X(m_its,2) - y(m_its))*X(m_its,2);
+%             end
+   
     
-    for m_its = 1:m
-        sum_theta_one = sum_theta_one + ( theta(1) + theta(2)*X(m_its,2) - y(m_its))*X(m_its,2);
-    end
+    sum_theta_zero = sum((theta(1) + theta(2)*X(:,2) - y).*X(:,1));
+    sum_theta_one = sum( (theta(1) + theta(2)*X(:,2) - y).*X(2,2));
     
-       
-     temp1 = theta(1) - alpha*(1/m)*sum_theta_zero;
-     temp2 = theta(2) - alpha*(1/m)*sum_theta_one;
-     
-     theta(1) = temp1;
-     theta(2) = temp2;
+    theta1 = theta(1) - alpha * (1/m) * sum((X*theta-y).*X(:,1));
+    theta2 = theta(2) - alpha * (1/m) * sum((X*theta-y).*X(:,2));
+
+
+%      temp1 = theta(1) - alpha*(1/m)*sum_theta_zero;
+%      temp2 = theta(2) - alpha*(1/m)*sum_theta_one;
+      
+      theta(1) = theta1;
+      theta(2) = theta2;
      
     J_history(iter) = computeCost(X, y, theta);
     
